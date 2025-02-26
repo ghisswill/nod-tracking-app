8efe4aa0b810dc8afecd034362d19d36eb52a52d
« ghisswill» <kkghislain@hotmail.fr>
2025-02-26 17:21:09 +0100

    feat: ajout des actions sur le user

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { UserService } from '../../shared/services/user/user.service';
 import { User } from '../../shared/models/user.model';
@@ -17,21 +17,44 @@ export class RegisterComponent implements OnInit {
   formCreate!: FormGroup;
   invalidUserToCreate: boolean = false;
   createUserSubscription: Subscription | null = null;
+  title: string = "Create an Account";
+  userId!: number;
+  user?: User;
 
   constructor(
+    private route: ActivatedRoute,
     private router: Router,
     private fb: FormBuilder,
     private userService: UserService) { }
 
   ngOnInit() {
-    this.formCreate = this.fb.group({
-      lastName: ['', [Validators.required]],
-      firstName: ['', [Validators.required]],
-      username: ['', [Validators.required]],
-      password: ['', Validators.required, Validators.email],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
-    });
+    this.userId = parseInt(this.route.snapshot.queryParams['id']);
+    if (this.userId) {
+      this.userService.getuser(this.userId).subscribe({
+        next: item => {
+          this.user = item;
+        }
+      })
+      this.title = "Edit User's profile";
+      this.formCreate = this.fb.group({
+        lastName: this.user?.lastName,
+        firstName: this.user?.firstName,
+        username: this.user?.username,
+        password: this.user?.password,
+        email: this.user?.email,
+        phone: this.user?.phone,
+      });
+    } else {
+      this.formCreate = this.fb.group({
+        lastName: ['', [Validators.required]],
+        firstName: ['', [Validators.required]],
+        username: ['', [Validators.required]],
+        password: ['', Validators.required, Validators.email],
+        email: ['', [Validators.required, Validators.email]],
+        phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      });
+
+    }
   }
 
   onCreatUser() {
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { RegisterComponent } from "../register/register.component";
 import { User } from '../../shared/models/user.model';
-import { UserData } from '../../shared/models/mock/user.data';
 import { UserService } from '../../shared/services/user/user.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RegisterComponent],
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent implements OnInit{
+export class UserComponent implements OnInit {
 
-  users: User[]= [];
+  users: User[] = [];
 
-  constructor(private userService: UserService) {}
-ngOnInit(): void {
-  this.userService.getUsers().subscribe((res: User[]) => {
-    this.users = res;
-  });
-}
-deleteUser() {
-throw new Error('Method not implemented.');
-}
+  constructor(private userService: UserService) { }
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((res: User[]) => {
+      this.users = res;
+    });
+  }
+  deleteUser(user: User) {
+    if (user.userId)
+      this.userService.delete(user.userId)
+  }
 
 }
