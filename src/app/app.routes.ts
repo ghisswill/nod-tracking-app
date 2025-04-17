c59b1b2093232bc7cddaa53460f95c2c654ae8a5
« ghisswill» <kkghislain@hotmail.fr>
2025-04-17 12:17:28 +0200

    feat: ajout biblio primeng et muse a jour des differentes pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { RegisterComponent } from './components/register/register.component';
 import { UserComponent } from './components/user/user.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 export const routes: Routes = [
     {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
+    {path: 'demo', component: DemoComponent},
+    {path: 'register', component: RegisterComponent, canActivate: [authGuard]},
     {path: 'users', component: UserComponent, canActivate: [authGuard]},
     {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
