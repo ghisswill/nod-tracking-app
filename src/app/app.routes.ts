c523aa39ac87a7f55fd662672de592f457258a27
« ghisswill» <kkghislain@hotmail.fr>
2025-04-17 12:25:48 +0200

    fix: bug page demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,10 @@ import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { RegisterComponent } from './components/register/register.component';
 import { UserComponent } from './components/user/user.component';
-import { DemoComponent } from './components/demo/demo.component';
 
 export const routes: Routes = [
     {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
     {path: 'login', component: LoginComponent},
-    {path: 'demo', component: DemoComponent},
     {path: 'register', component: RegisterComponent, canActivate: [authGuard]},
     {path: 'users', component: UserComponent, canActivate: [authGuard]},
     {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
